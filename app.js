5c03e4ecc88c9e53990fc59af76b319db1a7fdb9
gary <gary.white@lodgit.com>
2022-05-03 13:16:31 +0200

    code working

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 function solution(nums) {
-
+    if (nums == null) {
+        return [];
+    } else {
+        nums = nums.sort((a, b) => a - b);
+        return nums;
+    }
 }
 
-console.log(solution(1, 2, 3, 10, 5)); // [1,2,3,5,10]
-console.log(solution(null)); // []
-console.log(solution()); // []
-console.log(solution(20, 2, 10)); // [2,10,20]
-console.log(solution(2, 20, 10)); // [2,10,20]
+console.log(solution([1, 2, 3, 10, 5])); // [1,2,3,5,10]
+console.log(solution([null])); // []
+console.log(solution([])); // []
+console.log(solution([20, 2, 10])); // [2,10,20]
+console.log(solution([2, 20, 10])); // [2,10,20]
